16de508283201b703c23a619880a4e300c9f310b
Muh Ghazali Akbar <muhghazaliakbar@icloud.com>
2023-07-05 04:35:53 +0800

    Update css framework to Tailwind CSS + Daisy UI

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="light">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.1.7/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     @include('navbar')
 
-    @yield('content')
+    <main class="container max-w-5xl mx-auto px-2 py-10">
+        @yield('content')
+    </main>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,35 +5,5 @@
 @endsection
 
 @section('content')
-<div class="container p-5">
-    <!-- Ini untuk menampilkan data yang banyak -->
-    @foreach($semuaArtikel as $artikel)
-        <div class="card w-full mb-3">
-            <div class="card-body">
-                <h5 class="card-title">{{ $artikel->judul }}</h5>
-                
-                <p class="card-text">Jumlah like: {{ $artikel->jumlah_like }}</p>
-                
-                <p class="card-text">{{ $artikel->konten }}</p>
-                
-                <a href="#" class="btn btn-primary">Detail</a>
-            </div>
-        </div>
-    @endforeach
-
-    <!-- Ini untuk menampilkan 1 data -->
-    <div class="card-body">
-        <h5 class="card-title">{{ $semuaArtikel->judul }}</h5>
-        
-        <p class="card-text">Jumlah like: {{ $semuaArtikel->jumlah_like }}</p>
-        
-        <p class="card-text">{{ $semuaArtikel->konten }}</p>
-        
-        <a href="#" class="btn btn-primary">Detail</a>
-    </div>
-    
-    
-    <!-- Bisa juga menggunakan @include dengan mengirim data yang diterima dari router -->
-    @include('blog-card', ['artikel' => $artikel])
-</div>
+    <h1>Welcome</h1>
 @endsection
